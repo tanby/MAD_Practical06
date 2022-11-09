ceacd1c247d33633f9fbbff812494ef4c0520221
BY <tan_boon_yuen@ichat.sp.edu.sg>
2022-11-10 07:56:04 +0800

    added Task 2 starter

---
+++ Task2/App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {Page1, Page2, Page3, Page4} from './Pages'
+
+const App = ()=>{
+  //-- add code for Task 2
+  //-- modify Pages.js 
+  
+  return (
+    //-- change the following pages to test different pages
+    <View>
+      <Page1 />
+      
+    </View>
+  )
+}
+
+export default App
---
+++ Task2/Pages.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Page1 = ()=>{
+  //-- add code for Task 2
+  
+  return (
+    <View>
+      <Text>This is Page 1</Text>
+    </View>
+  )
+}
+
+const Page2 = ()=>{
+  //-- add code for Task 2
+  
+  return (
+    <View>
+      <Text>This is Page 2</Text>
+    </View>
+  )
+}
+
+const Page3 = ()=>{
+  //-- add code for Task 2
+  
+  return (
+    <View>
+      <Text>This is Page 3</Text>
+    </View>
+  )
+}
+
+const Page4 = ()=>{
+  //-- add code for Task 2
+  
+  return (
+    <View>
+      <Text>This is Page 4</Text>
+    </View>
+  )
+}
+
+export {Page1, Page2, Page3, Page4}
